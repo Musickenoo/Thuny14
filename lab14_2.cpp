6a50f29e3b9a479855fa00182894217d47b0ad22
Thunyatorn Kongmoung <Thunyatorn1125@gmail.com>
2024-01-05 16:04:26 +0700

    lab14

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,33 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool a[][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void inputMatrix(double b[][N]){
+	for(int i = 0; i < N; i++){
+        cout << "Row " << i+1 << ": ";
+        for(int j = 0; j < N; j++){
+            cin >> b[i][j];
+        }
+    }
+}
+
+void findLocalMax(const double c[][N], bool d[][N]){
+	for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if( j >=1 && j <=N-2 && i >= 1 && i <=N-2 && c[i][j] >= c[i+1][j] &&
+			 c[i][j] >= c[i-1][j] && c[i][j] >= c[i][j-1] && c[i][j] >= c[i][j+1]){
+				d[i][j] = true;
+			}
+			else d[i][j] = false;
+        }
+    }
+}
